86105bea3ec6744d70cac2a2b642a8ab0a21b509
Lorenzo Malagia <lorenzo.malagia@gmail.com>
2014-10-22 19:25:56 +0200

    Components updated to SP 2

--- firmware.cydsn/command_processing.c
+++ firmware.cydsn/command_processing.c
@@ -846,9 +846,9 @@ void memInit(void)
     uint8 i;
     //initialize memory settings
     g_mem.id            =   1;
-    g_mem.k_p           =   0.1 * 65536;   //XXXXXXXXX
+    g_mem.k_p           =   0.01 * 65536;   //XXXXXXXXX
     g_mem.k_i           =   0 * 65536;
-    g_mem.k_d           =   0.8 * 65536;
+    g_mem.k_d           =   0 * 65536;
     g_mem.activ         =   0;
     g_mem.input_mode    =   0;
     g_mem.control_mode  =   0;
