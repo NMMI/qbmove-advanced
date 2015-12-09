564600d592895e78f092019f63033b8d68a3edfa
qbrobotics <info@qbrobotics.com>
2015-12-09 12:28:59 +0100

    v5.3.1 version alignment

--- firmware.cydsn/globals.h
+++ firmware.cydsn/globals.h
@@ -28,7 +28,7 @@
 //                                                                        DEVICE
 //==============================================================================
 
-#define VERSION         "QBADV v5.2.1"
+#define VERSION         "QBADV v5.3.1"
 
 #define NUM_OF_MOTORS           2
 #define NUM_OF_SENSORS          3
