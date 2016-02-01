3715435c79a73152528a8afe1b75ce66e9412175
qbrobotics <info@qbrobotics.com>
2016-02-01 16:51:06 +0100

    v5.4.1 Changed current scale factor (calculated according datasheet's values) Modified current filters due to approximation error while filtering

--- firmware.cydsn/globals.h
+++ firmware.cydsn/globals.h
@@ -28,7 +28,7 @@
 //                                                                        DEVICE
 //==============================================================================
 
-#define VERSION         "QBADV v5.4.0"
+#define VERSION         "QBADV v5.4.1"
 
 #define NUM_OF_MOTORS           2
 #define NUM_OF_SENSORS          3
